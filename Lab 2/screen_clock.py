b6a92ada1f920267d2934419f93603ca7a26917d
alan7622 <alan7622@gmail.com>
2022-09-11 20:42:49 -0400

    add clockWise and counterClockWise function

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -5,6 +5,7 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
+
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
@@ -48,27 +49,80 @@ padding = -2
 top = padding
 bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
-x = 0
+
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
 # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font1 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 40)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-while True:
+# Set buttom
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+
+x1 = 0
+x2 = 0
+y1=5
+y2=30
+
+def clockWise():
     # Draw a black filled box to clear the image.
+    global x1, x2, y1, y2    
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    if x1 < 130 and x2 < 130 and y1 == 5 and y2 == 30:
+        x1 += 10
+        x2 += 10
+    elif y1 < 70 and y2 < 95 and x1 == 130 and x2 == 130:
+        y1 += 5
+        y2 += 5
+    elif y1 == 70 and y2 == 95 and x1 > 0 and x2 > 0:
+        x1 -= 10
+        x2 -= 10
+    elif x1 == 0 and x2 == 0 and y1 <= 70 and y2 <= 95:
+        y1 -= 5
+        y2 -= 5
+
+    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+    draw.text((x1,y1), time.strftime("%a %d" ), font=font1, fill="#FFFF9C")
+    draw.text((x2,y2), time.strftime("%H:%M"), font=font2, fill="#AEFF9C")
+
+def counterClockWise():
+    # Draw a black filled box to clear the image.
+    global x1, x2, y1, y2    
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-    clocks = time.strftime("%m/%d/%Y %H:%M:%S")
+    if x1 == 0 and x2 == 0 and y1 < 70 and y2 < 95:
+        y1 += 5
+        y2 += 5
+    elif y1 == 70 and y2 == 95 and x1 < 130 and x2 < 130:
+        x1 += 10
+        x2 += 10
+    elif x1 <= 130 and x2 <= 130 and y1 == 0 and y2 == 25:
+        x1 -= 10
+        x2 -= 10
+    elif y1 <= 70 and y2 <= 95 and x1 == 130 and x2 == 130:
+        y1 -= 5
+        y2 -= 5
 
-    y = top
-    draw.text((x,y),clocks, font=font,fill="#FFFFFF")
+
+    #TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+    draw.text((x1,y1), time.strftime("%a %d" ), font=font1, fill="#9CD9FF")
+    draw.text((x2,y2), time.strftime("%H:%M"), font=font2, fill="#FF9CDB")
+
+while True:
+    if not buttonA.value:
+        clockWise()
+    if not buttonB.value:
+        counterClockWise()
     # Display image.
     disp.image(image, rotation)
-    time.sleep(1)
+    time.sleep(0.01)
